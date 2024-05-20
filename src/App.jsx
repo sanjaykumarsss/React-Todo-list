56001fc323aca92a86aea598801773b81139cb32
sanjay <sanjaykumarsiva16@gmail.com>
2024-05-20 12:13:23 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Todolist from './component/Todolist'
+
 import Todo from './component/Todo'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
 
   return (
    <>
-   {/* <Todolist/> */}
+ 
    <Todo/>
    </>
   )
